946c1c2d95d5143ac99521366df5d5e8e0f44124
ghdgu <ghdguswl13@gmail.com>
2024-07-22 17:28:59 +0900

    feat: FAQ

--- SpaceCreation/src/main/java/com/aws/spacecreation/answer/Answer.java
+++ SpaceCreation/src/main/java/com/aws/spacecreation/answer/Answer.java
@@ -11,8 +11,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class Answer {
 @Id
