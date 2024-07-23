205fd62867b5579f7918e0d9556ff00fa153d4e8
haapppy23 <sh6273sh@gmail.com>
2024-07-23 17:42:33 +0900

    디자인 변경 버전

--- SpaceCreation/src/main/java/com/aws/spacecreation/user/UserService.java
+++ SpaceCreation/src/main/java/com/aws/spacecreation/user/UserService.java
@@ -1,14 +1,18 @@
 package com.aws.spacecreation.user;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import static com.aws.spacecreation.user.UserRole.ADMIN;
+import static com.aws.spacecreation.user.UserRole.USER;
 
 import java.util.Optional;
 
-import static com.aws.spacecreation.user.UserRole.USER;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-@Service@RequiredArgsConstructor
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -22,23 +26,37 @@ public class UserService {
         this.userRepository.save(user);
     }
 
-    public void update(String username, String email, String password){
+    public void update(String username, String email, String password) {
         Optional<SiteUser> userOpt = userRepository.findByUsername(username);
-        if(userOpt.isPresent()){
+        if (userOpt.isPresent()) {
             SiteUser user = userOpt.get();
             user.setUsername(username);
             user.setEmail(email);
             user.setPassword(passwordEncoder.encode(password));
             userRepository.save(user);
-        }
-        else{
+        } else {
             throw new IllegalArgumentException("사용자가 존재하지않습니다");
         }
-
     }
 
-    public void withdraw(SiteUser siteUser){
-
+    public void withdraw(SiteUser siteUser) {
         userRepository.delete(siteUser);
+    }
+
+    @PostConstruct
+    public void init() {
+        createAdminIfNotExists();
+    }
+
+    private void createAdminIfNotExists() {
+        Optional<SiteUser> adminOpt = userRepository.findByUsername("admin");
+        if (adminOpt.isEmpty()) {
+            SiteUser admin = new SiteUser();
+            admin.setUsername("admin");
+            admin.setEmail("admin@example.com");
+            admin.setPassword(passwordEncoder.encode("admin123")); // 기본 비밀번호
+            admin.setUserRole(ADMIN);
+            this.userRepository.save(admin);
+        }
     }
 }
